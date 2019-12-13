48338f5d28987dcdae2359f788910c04300f353c
MinexAutomation <minex.automation@gmail.com>
2019-12-12 16:44:59 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Patras.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Patras.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
